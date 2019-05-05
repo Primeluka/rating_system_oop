6063536aaf00d44d82ddc10d18fc73162d32a248
lukasz.nedzka <nedzka.lukasz@gmail.com>
2019-05-05 12:51:09 +0200

    Deleted unnecessary classes

--- core/init.php
+++ core/init.php
@@ -1,17 +1,4 @@
 <?php
-/*
-   try
-   {
-      $pdo = new PDO('mysql:host=localhost;dbname=rating_system', 'root', '');
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $pdo -> exec("SET CHARACTER SET utf8");
-   }
-   catch(PDOException $e)
-   {
-      echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage();
-      die();
-   }
-*/
 
    $GLOBALS['config'] = array(
 
